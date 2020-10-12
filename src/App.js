5675e89c76f106af608b1dd5402ef6a5b2b8a419
Diego Cravo Teixeira <cravo.diego@gmail.com>
2020-10-12 05:35:03 -0300

    pages add

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import React from 'react';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './App.css';
 import NavBar from './components/layout/NavBar';
 import Dashboard from './components/layout/Dashboard';
+import Pokemon from './components/pokemon/Pokemon';
 
 import backgroundImage from './pattern.png';
 
 function App() {
   return (
-    <div className="App" style={{ background: `url(${backgroundImage})`}}>
+    <Router>
+      <div className="App" style={{ background: `url(${backgroundImage})`}}>
           <NavBar />
           <div className='container'>
-            <Dashboard />
+            <Switch>
+              <Route exact path='/' component={ Dashboard} />
+              <Route exact path='/pokemon/:pokemonIndex' component={ Pokemon} />
+            </Switch>
           </div>
-
-    </div>
+      </div>
+    </Router>
+    
   );
 }
 
--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 
 import styled from 'styled-components';
 
@@ -21,6 +22,18 @@ const Card = styled.div`
     user-select: none;
     -o-user-select:none;
 `;
+
+const StyledLink = styled(Link)`
+    text-decoration: none;
+    color: black;
+    &:focus,
+    &:hover,
+    &:visited,
+    &:link,
+    &:active {
+        text-decoration:none;
+    }
+`
 
 export default class Pokemoncard extends Component {
     state = {
@@ -47,40 +60,42 @@ export default class Pokemoncard extends Component {
     render() {
         return (
             <div className="col-md-3 col-sm-6 mb-5">
-                <Card className="card">
-                    <h5 className='card-header'>{this.state.pokemonIndex}</h5>
-                    {this.state.imageloading ? (
-                        <img src={spinner} style={{width: '5em', height: '5em'}} className='card-img-top rounded mx-auto d-block mt-2'></img>
-                    ) : null}
-                    <Sprite 
-                        className='card-img-top rounded mx-auto mt-2'
-                        onLoad={ () => this.setState({imageloading: false})}
-                        onError={ () => this.setState({toManyRequests: true})}
-                        src={this.state.imageUrl}
-                        style={
-                            this.state.toManyRequests ? {display: 'none'} :
-                            this.state.imageloading ? null : {display: 'block'}
-                        }
-                    />
-                    {this.state.toManyRequests ? (<h6 className='mx-auto'>
-                        <span className='badge badge-danger mt-2'>
-                            Too Many Requests
-                        </span>
-                    </h6>) : null}
-                   
-                    <div className='card-body  mx-auto'>
-                    <h6 className='card-title'>
-                        {this.state.name
-                            .toLowerCase()
-                            .split(' ')
-                            .map(
-                                letter => letter.charAt(0).toUpperCase() + letter.substring(1)
-                        )
-                        .join(' ')
-                        }
-                    </h6>
-                </div>
-                </Card>
+                <StyledLink to={`pokemon/${this.state.pokemonIndex}`}>
+                    <Card className="card">
+                        <h5 className='card-header'>{this.state.pokemonIndex}</h5>
+                        {this.state.imageloading ? (
+                            <img src={spinner} style={{width: '5em', height: '5em'}} className='card-img-top rounded mx-auto d-block mt-2'></img>
+                        ) : null}
+                        <Sprite 
+                            className='card-img-top rounded mx-auto mt-2'
+                            onLoad={ () => this.setState({imageloading: false})}
+                            onError={ () => this.setState({toManyRequests: true})}
+                            src={this.state.imageUrl}
+                            style={
+                                this.state.toManyRequests ? {display: 'none'} :
+                                this.state.imageloading ? null : {display: 'block'}
+                            }
+                        />
+                        {this.state.toManyRequests ? (<h6 className='mx-auto'>
+                            <span className='badge badge-danger mt-2'>
+                                Too Many Requests
+                            </span>
+                        </h6>) : null}
+                    
+                        <div className='card-body  mx-auto'>
+                        <h6 className='card-title'>
+                            {this.state.name
+                                .toLowerCase()
+                                .split(' ')
+                                .map(
+                                    letter => letter.charAt(0).toUpperCase() + letter.substring(1)
+                            )
+                            .join(' ')
+                            }
+                        </h6>
+                    </div>
+                    </Card>
+                </StyledLink>
             </div>
         )
     }
